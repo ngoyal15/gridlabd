9f63bfd22512e97224ffd3d6630d60045156d5b1
aivanova5 <ivanova.alyona5@gmail.com>
2019-04-24 15:19:28 -0700

    Small edits to makefile.mk

--- gldcore/link/python/Makefile.mk
+++ gldcore/link/python/Makefile.mk
@@ -1,8 +1,8 @@
 python-install:
-	@echo "python3 $(top_srcdir)/gldcore/link/python/setup.py --quiet install"
+	@echo "python $(top_srcdir)/gldcore/link/python/setup.py --quiet install"
 	@( export SRCDIR=$(top_srcdir) ; python3 $(top_srcdir)/gldcore/link/python/setup.py --quiet install )
 
 python-clean:
-	@echo "python3 $(top_srcdir)/gldcore/link/python/setup.py clean"
+	@echo "python $(top_srcdir)/gldcore/link/python/setup.py clean"
 	@( export SRCDIR=$(top_srcdir) ; python3 $(top_srcdir)/gldcore/link/python/setup.py clean )
 	-rm -rf $(SRCDIR)/build/lib.*
