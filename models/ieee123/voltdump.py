8eeed8f609121ddc76981bbd325ca56b4e9d8725
ngoyal15 <52181849+ngoyal15@users.noreply.github.com>
2019-07-15 10:57:50 -0700

    Update voltdump.py

--- models/ieee123/voltdump.py
+++ models/ieee123/voltdump.py
@@ -55,8 +55,7 @@ with open ("config/model.glm","r") as checking_model:
 	#	if(i== 7):
 	#		print("Inside the while loop")
 	word = specific.split('_')[1]
-	print(word)
-	model_number = word[0:7]
+	model_number = word[0:1]
 	path_voltages = "output/final_output/model_"+model_number+"/voltages.csv"
 	path_powers = "output/final_output/model_"+model_number+"/powers.csv"
 	#		print(path_voltages)
